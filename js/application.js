d9a3c7da65a021a60035f55698b45d8b5a939da3
Diego De Souza <diegotrus7@gmail.com>
2015-02-06 23:53:31 -0500

    complete todo in ember

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
 // create new instance of Ember.Application
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
